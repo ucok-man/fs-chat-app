810a2d8a03f02cba06ffa89ebbf62aa1ac8d01d9
Alvin Odhi N <ucokkocu411@gmail.com>
2025-01-16 22:47:40 +0700

    fix (backend): removing test error healthcheck handler

--- backend/cmd/api/handler_healtcheck.go
+++ backend/cmd/api/handler_healtcheck.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	app.errServerResponse(w, r, errors.New("AAA"))
-	return
-	// env := envelope{
-	// 	"status": "available",
-	// 	"system_info": map[string]string{
-	// 		"environment": app.config.env,
-	// 		"version":     version,
-	// 	},
-	// }
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
 
-	// err := app.writeJSON(w, http.StatusOK, env, nil)
-	// if err != nil {
-	// 	app.errServerResponse(w, r, err)
-	// 	return
-	// }
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.errServerResponse(w, r, err)
+		return
+	}
 }
